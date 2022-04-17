4e6759da68a4cfe96227128965de5300b105efc2
Pope Scooby <PopeScooby@gmail.com>
2022-04-17 18:00:48 -0500

    Deliv 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,3 +1,4 @@
+#Deliverable 1
 library(dbplyr)
 
 mechaCarMpg <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)#Import The CSV
@@ -6,3 +7,9 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=mechaCarMpg))#Determine P Value and R**
 
+#Deliverable 2
+suspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)#Import The CSV
+
+summaryTotal <- suspensionCoil %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
+
+summaryLot <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
