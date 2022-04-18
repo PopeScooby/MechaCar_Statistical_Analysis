4af1319e0e86dc3eb8764387be4d24271377c073
Pope Scooby <PopeScooby@gmail.com>
2022-04-17 19:56:06 -0500

    deliv 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -10,6 +10,15 @@ summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_cleara
 #Deliverable 2
 suspensionCoil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F)#Import The CSV
 
-summaryTotal <- suspensionCoil %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
+summaryTotal <- suspensionCoil %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))# Total Summary
 
-summaryLot <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))
+summaryLot <- suspensionCoil %>% group_by(Manufacturing_Lot) %>% summarize(mean = mean(PSI), median = median(PSI), variance = var(PSI), sd = sd(PSI))# Lot Summary
+
+#Deliverable 3
+t.test(x=suspensionCoil$PSI,mu=1500) #Total Mean 
+
+t.test(x=subset(suspensionCoil,Manufacturing_Lot=="Lot1")$PSI, mu=1500) #Lot1 T.Test 
+
+t.test(x=subset(suspensionCoil,Manufacturing_Lot=="Lot2")$PSI, mu=1500) #Lot2 T.Test 
+
+t.test(x=subset(suspensionCoil,Manufacturing_Lot=="Lot3")$PSI, mu=1500) #Lot3 T.Test 
